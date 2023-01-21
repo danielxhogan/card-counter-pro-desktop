f956cf15b506c29369938a08622d3a009f2bc628
danielxhogan <danielxhogan@gmail.com>
2023-01-20 16:18:11 -0800

    display an image in the ui

--- src/ui/centralwidget.cpp
+++ src/ui/centralwidget.cpp
@@ -1,15 +1,42 @@
 #include "centralwidget.h"
+#include "env.h"
 
 #include <QGridLayout>
+#include <QImage>
+#include <QPixmap>
+#include <QString>
+#include <QLabel>
+
+#include <iostream>
+#include <format>
 
 CentralWidget::CentralWidget(QWidget* parent) :
 	QWidget(parent)
 {
+	QPixmap* image = new QPixmap(100, 300);
+	QString image_dir = INSTALL_IMAGES_DIR;
+	image_dir.append("/cards/01_of_spades_A.svg.png");
+
+	// attempts to get image from install dir first
+	// if not found, checks buld dir
+	if (!image->load(image_dir)) {
+		image_dir = BUILD_IMAGES_DIR;
+		image_dir.append("/cards/01_of_spades_A.svg.png");
+		image->load(image_dir);
+	}
+
+	//QImage* image = new QImage(image_dir);
+
+	QLabel* image_label = new QLabel;
+	image_label->setPixmap(*image);
+
+
 	create_test_combo();
 	
 	QGridLayout* layout = new QGridLayout;
 	layout->addWidget(test_label, 0, 0);
 	layout->addWidget(test_combo, 0, 1);
+	layout->addWidget(image_label, 0, 3);
 	
 	setLayout(layout);
 }
